a0b344935d32d982a3cbe31a5cc369a1e0ee0fbd
Jiwon Jeoung <zwondd@gmail.com>
2018-09-30 10:37:00 +0900

    도전프로그래밍 1번, 2번 commit

--- CProgramming/YoonChallenge3-1.c
+++ CProgramming/YoonChallenge3-1.c
@@ -1,65 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void PrintSnailArray(int n)
+/*
+	열혈 C : 도전 프로그래밍3
+	1) 길이가 4X4 인 int 형 2차원 배열 선언, 배열의 요소들을 오른쪽 방향으로 90도씩 이동시켜 그 결과를 출력
+*/
+
+void RotateArray(int(*ptr)[4])
 {
-	int **visitArr, **arr;
-	int dir = 0;
-	int cur_x = 0;
-	int cur_y = 0;
-	int x[] = { 0,1,0,-1 };
-	int y[] = { 1,0,-1,0 }; 
+	int tempArr[4][4];
 
-	// visited 배열 메모리 할당
-	visitArr = (int**)malloc(sizeof(int*)*n);
-	for (int i = 0; i < n; i++)
-		visitArr[i] = (int(*)) malloc(sizeof(int)*n);
-
-	// arr 배열 메모리 할당
-	arr = (int**)malloc(sizeof(int*)*n);
-	for (int i = 0; i < n; i++)
-		arr[i] = (int(*)) malloc(sizeof(int)*n);
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			visitArr[i][j] = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			tempArr[j][3 - i] = ptr[i][j];
 		}
 	}
 
-	int tmp = 0;
-	while (tmp < n*n) {
-		visitArr[cur_x][cur_y] = 1;
-		arr[cur_x][cur_y] = tmp + 1;
-		tmp++;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+			ptr[i][j] = tempArr[i][j];
+}
 
-		int next_x = cur_x + x[dir % 4];
-		int next_y = cur_y + y[dir % 4];
-
-		if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n)
-			dir++;
-		else if (visitArr[next_x][next_y] == 1)
-			dir++;
-
-		cur_x = cur_x + x[dir % 4];
-		cur_y = cur_y + y[dir % 4];
-	}
-
-	// Print
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++)
-			printf("%2d  ", arr[i][j]);
+void PrintArray(int(*ptr)[4])
+{
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%2d   ", ptr[i][j]);
+		}
 		printf("\n");
 	}
+	printf("\n\n");
 }
 
-
-int main(void)
+int main()
 {
-	int n;
-	scanf_s("%d", &n);
-	printf("입력받은 숫자 : %d \n", n);
-	PrintSnailArray(n);
+	int arr[4][4];
+	int tmp = 1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			arr[i][j] = tmp++;
+		}
+	}
+	RotateArray(arr);
+	PrintArray(arr);
 
+	RotateArray(arr);
+	PrintArray(arr);
 
+	RotateArray(arr);
+	PrintArray(arr);
 }
-
